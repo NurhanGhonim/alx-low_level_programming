86a570f42d3a29833ea988331f4f7e1e82a4057f
NurhanGhonim <nurhanghoniem487@gmail.com>
2023-10-20 08:02:13 -0700

    mm

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /*
- *  * Desc: Header file containing declarations for all functions
- *   *       used in the 0x02-functions_nested_loops directory.
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
  */
 
 int _putchar(char c);
