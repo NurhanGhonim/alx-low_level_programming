ccbc60f78a944f211cc8b6ed9c42bd397484411d
NurhanGhonim <nurhanghoniem487@gmail.com>
2023-10-21 07:52:54 -0700

    hh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Write a program that prints all single digits from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
