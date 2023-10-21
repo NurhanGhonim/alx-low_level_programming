7a873ebda4a3ccab82a1d0c55e7aa3d84cac0c3d
NurhanGhonim <nurhanghoniem487@gmail.com>
2023-10-21 07:37:03 -0700

    dd

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,20 @@
 /**
  *main - entry point
  *
- * Description: prints all single digit numbers of base 10 starting from 0
+ * Description: prints all single digit numbers
  *
  * Return: 0 (success)
 */
 
 int main(void)
 {
-		int digit = 0;
+	int digit = 0;
 
-		while (digit <= 9)
-		{
-			printf("%i", digit);
-		}
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
 		printf("\n");
 		return (0);
 }
