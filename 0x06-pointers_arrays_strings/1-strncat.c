dc1632498f344bc77c7a7839ee48941c19a9c155
NurhanGhonim <nurhanghoniem487@gmail.com>
2023-10-23 07:32:40 -0700

    kk

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,22 +7,24 @@
  * @n: number of bytes of str to concatenate
  *
  * Return: a pointer to the resulting string dest
- */
+*/
+
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
+	int i, j;
 
 	i = 0;
 	j = 0;
 
-while (dest[i] != '\0')
+	while (dest[i] != '\0')
 	i++;
 
 	while (src[j] != '\0' && j < n)
-	{										dest[i] = src[j];
-	i++;
-	j++;
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
-return (dest);
+	return (dest);
 }
